096de561968379f7c979c6d1bf530ba65ad3ef71
Roman Sluka <r.sluka@itransition.com>
2022-07-03 18:30:12 +0400

    Added basic configuration.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    hot: false,
+  },
+};
